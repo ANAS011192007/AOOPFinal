c9db5c50764de4c39c9c72c622e793654bd36037
ANAS011192007 <52394372+ANAS011192007@users.noreply.github.com>
2021-02-02 11:25:38 +0600

    Update Snake.java

--- src/aoop/Snake.java
+++ src/aoop/Snake.java
@@ -26,9 +26,9 @@ public class Snake{
 class Game extends JPanel implements ActionListener,KeyListener{
     BufferedWriter writer;
     BufferedReader reader;
-    int[] xpos=new int[750];
-    int[] ypos=new int[750];
-    int m=0,sl=3,t=100,count=0;
+    int[] snakexpos=new int[750];
+    int[] snakeypos=new int[750];
+    int start=0,snakelength=3,count=0;
     boolean l=false;boolean r=false;boolean u=false;boolean d=false;
     int objectxpos[]={25,50,75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,775,800,825,850};
     int objectypos[]={75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,625};
@@ -45,7 +45,7 @@ class Game extends JPanel implements ActionListener,KeyListener{
        setFocusable(true);
        addKeyListener(this);
        setFocusTraversalKeysEnabled(false);
-       Timer time=new Timer(t,this);
+       Timer time=new Timer(100,this);
        time.start();
     }    
     public String getSname() {
@@ -70,51 +70,51 @@ class Game extends JPanel implements ActionListener,KeyListener{
         Font f4=new Font("Helvetica",Font.ITALIC,14);
         G.setFont(f4);
         G.drawString("Player = "+getSname(),750,30);
-        if(m==0){
-            xpos[0]=100;
-            xpos[1]=75;
-            xpos[2]=50;
-            ypos[0]=100;
-            ypos[1]=100;
-            ypos[2]=100;
+        if(start==0){
+            snakexpos[0]=100;
+            snakexpos[1]=75;
+            snakexpos[2]=50;
+            snakeypos[0]=100;
+            snakeypos[1]=100;
+            snakeypos[2]=100;
         }
         ImageIcon lm,dm,um,sb;
         ImageIcon rm=new ImageIcon("E://rm.png");  
-        rm.paintIcon(this,G,xpos[0],ypos[0]);
-        for(int i=0;i<sl;i++){
+        rm.paintIcon(this,G,snakexpos[0],snakeypos[0]);
+        for(int i=0;i<snakelength;i++){
             if(i==0 && l){
                 lm=new ImageIcon("E://lm.png");
-                lm.paintIcon(this,G,xpos[i],ypos[i]);          
+                lm.paintIcon(this,G,snakexpos[i],snakeypos[i]);          
             }
             if(i==0 && r){
                 rm=new ImageIcon("E://rm.png");
-             rm.paintIcon(this,G,xpos[i],ypos[i]);
+             rm.paintIcon(this,G,snakexpos[i],snakeypos[i]);
             }
             if(i==0 && u){
                 um=new ImageIcon("E://um.png");
-             um.paintIcon(this,G,xpos[i],ypos[i]);
+             um.paintIcon(this,G,snakexpos[i],snakeypos[i]);
             }
             if(i==0 && d){
                  dm=new ImageIcon("E://dm.png");
-             dm.paintIcon(this,G,xpos[i],ypos[i]);
+             dm.paintIcon(this,G,snakexpos[i],snakeypos[i]);
             }
             if(i!=0){
                 sb=new ImageIcon("E://sb.png");
-             sb.paintIcon(this,G,xpos[i],ypos[i]);
+             sb.paintIcon(this,G,snakexpos[i],snakeypos[i]);
             }
         }
-        if(objectxpos[xr]==xpos[0]){
-            if(objectypos[yr]==ypos[0]){
-                     sl++;
+        if(objectxpos[xr]==snakexpos[0]){
+            if(objectypos[yr]==snakeypos[0]){
+                     snakelength++;
                      xr=n.nextInt(34);
                      yr=n.nextInt(23);
                      score++;
             }
         }
         points.paintIcon(this,G,objectxpos[xr],objectypos[yr]);
-        for(int i=1;i<sl;i++){
-            if(xpos[i]==xpos[0]){
-                if(ypos[i]==ypos[0]){
+        for(int i=1;i<snakelength;i++){
+            if(snakexpos[i]==snakexpos[0]){
+                if(snakeypos[i]==snakeypos[0]){
                     r=false;
                     l=false;
                     u=false;
@@ -142,65 +142,65 @@ class Game extends JPanel implements ActionListener,KeyListener{
     @Override
     public void actionPerformed(ActionEvent ae) {
         if(r){
-            for(int i=sl-1;i>=0;i--){
-                ypos[i+1]=ypos[i];
+            for(int i=snakelength-1;i>=0;i--){
+                snakeypos[i+1]=snakeypos[i];
             }
-            for(int i=sl;i>=0;i--){
+            for(int i=snakelength;i>=0;i--){
                 if(i!=0){
-                    xpos[i]=xpos[i-1];
+                    snakexpos[i]=snakexpos[i-1];
                 }else{
-                xpos[i]=xpos[i]+25;
+                snakexpos[i]=snakexpos[i]+25;
             }
-                if(xpos[i]>850){
-                    xpos[i]=25;
+                if(snakexpos[i]>850){
+                    snakexpos[i]=25;
                 }
             }
             repaint();
         }
         else if(l){
-            for(int i=sl-1;i>=0;i--){
-                ypos[i+1]=ypos[i];
+            for(int i=snakelength-1;i>=0;i--){
+                snakeypos[i+1]=snakeypos[i];
             }
-            for(int i=sl;i>=0;i--){
+            for(int i=snakelength;i>=0;i--){
                 if(i!=0){
-                    xpos[i]=xpos[i-1];
+                    snakexpos[i]=snakexpos[i-1];
                 }else{
-                xpos[i]=xpos[i]-25;
+                snakexpos[i]=snakexpos[i]-25;
             }
-                if(xpos[i]<25){
-                    xpos[i]=850;
+                if(snakexpos[i]<25){
+                    snakexpos[i]=850;
                 }
             }
             repaint();
         }
         else if(d){
-            for(int i=sl-1;i>=0;i--){
-                xpos[i+1]=xpos[i];
+            for(int i=snakelength-1;i>=0;i--){
+                snakexpos[i+1]=snakexpos[i];
             }
-            for(int i=sl;i>=0;i--){
+            for(int i=snakelength;i>=0;i--){
                 if(i!=0){
-                    ypos[i]=ypos[i-1];
+                    snakeypos[i]=snakeypos[i-1];
                 }else{
-                ypos[i]=ypos[i]+25;
+                snakeypos[i]=snakeypos[i]+25;
             }
-                if(ypos[i]>625){
-                    ypos[i]=75;
+                if(snakeypos[i]>625){
+                    snakeypos[i]=75;
                 }
             }
             repaint();
         }
         else if(u){
-            for(int i=sl-1;i>=0;i--){
-                xpos[i+1]=xpos[i];
+            for(int i=snakelength-1;i>=0;i--){
+                snakexpos[i+1]=snakexpos[i];
             }
-            for(int i=sl;i>=0;i--){
+            for(int i=snakelength;i>=0;i--){
                 if(i!=0){
-                    ypos[i]=ypos[i-1];
+                    snakeypos[i]=snakeypos[i-1];
                 }else{
-                ypos[i]=ypos[i]-25;
+                snakeypos[i]=snakeypos[i]-25;
             }
-                if(ypos[i]<75){
-                    ypos[i]=625;
+                if(snakeypos[i]<75){
+                    snakeypos[i]=625;
                 }
             }
             repaint();
@@ -213,7 +213,7 @@ class Game extends JPanel implements ActionListener,KeyListener{
     @Override
     public void keyPressed(KeyEvent ke) {
           if(ke.getKeyCode()==38 && restart==0){
-             m++;
+             start++;
              u=true;
              if(!d){
                  d=false;
@@ -226,7 +226,7 @@ class Game extends JPanel implements ActionListener,KeyListener{
              r=false;
         }
         else if(ke.getKeyCode()==40 && restart==0){
-             m++;
+             start++;
              d=true;
              if(!u){
                  u=false;
@@ -240,7 +240,7 @@ class Game extends JPanel implements ActionListener,KeyListener{
             
         }
         else if(ke.getKeyCode()==37 && restart==0){
-             m++;
+             start++;
              l=true;
              if(!r){
                  r=false;
@@ -253,7 +253,7 @@ class Game extends JPanel implements ActionListener,KeyListener{
              d=false;
         }
         else if(ke.getKeyCode()==39 && restart==0){
-             m++;
+             start++;
              r=true;
              if(!l){
                  l=false;
@@ -266,8 +266,8 @@ class Game extends JPanel implements ActionListener,KeyListener{
              d=false;
         }
           if(ke.getKeyCode()==KeyEvent.VK_ENTER && restart==1){
-              sl=3;
-              m=0;
+              snakelength=3;
+              start=0;
               score=0;
               restart=0;
               repaint();
